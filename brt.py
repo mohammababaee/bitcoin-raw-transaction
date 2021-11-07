4021c1c467e747e98aa4e47c1601a488618057e6
mohammad <mohammadbabaeesem@gmail.com>
2021-11-07 21:11:58 +0330

    Implement creating tx for sending bitcoin

--- brt.py
+++ brt.py
@@ -2,7 +2,8 @@ import secrets
 import codecs
 import hashlib
 import ecdsa
-from ecdsa.ecdsa import Public_key
+import struct
+import base58
 
 # -----------------------------------------------------------------------------------------------------------------------
 # The first step is creating a private key. use secrets library in python for creating a random 256-bit number
@@ -87,14 +88,14 @@ def create_address_from_public_key(public_key):
     checksum = sha256_second_time_hex[:8]
     add_checksum_to_network_bitcoin_public_key = network_bitcoin_public_key + checksum
     address_hex = add_checksum_to_network_bitcoin_public_key.decode('utf-8')
-    final_address = base58(address_hex)
+    final_address = base58_function(address_hex)
     return final_address
 
 
 # Function for converting to base58
 
 
-def base58(address_hex):
+def base58_function(address_hex):
     alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
     b58_string = ''
     # Get the number of leading zeros and convert hex to decimal
@@ -124,7 +125,126 @@ def create_address():
     print("=============================================================================")
 
 
-print("Sender Address")
-create_address()
-print("Receiver Address")
-create_address()
+# -----------------------------------------------------------------------------------------------------------------------
+'''Next step after creating addresses is Creating a transaction. Creating a transaction required to have funds in your address for that purpose you can use a bitcoin testnet faucet
+# 1. raw tx
+# 2. raw tx and sign that tx using my private key 
+# 3. raw tx and signature in order to create the real tx  '''
+
+sender_addr = 'sender_address'
+sender_hashed_pubkey = base58.b58decode_check(sender_addr)[1:].hex()
+sender_private_key = 'sender_private_key'
+
+receiver_adr = 'receiver_address'
+receiver_hashed_pubkey = base58.b58decode_check(
+    receiver_adr)[1:].hex()
+
+prv_txid = 'previous transaction id'
+
+
+class raw_tx:
+    version = struct.pack("<L", 1)
+    tx_in_count = struct.pack("<B", 1)
+    tx_in = {}  # TEMP
+    tx_out_count = struct.pack("<B", 2)
+    tx_out1 = {}  # TEMP
+    tx_out2 = {}  # TEMP
+    lock_time = struct.pack("<L", 0)
+
+# We use this function for flip bytes because of little endian and big endian
+
+
+def flip_byte_order(string):
+    flipped = "".join(reversed([string[i:i+2]
+                      for i in range(0, len(string), 2)]))
+    return flipped
+
+
+rtx = raw_tx()  # Create a new child from raw_tx class
+
+rtx.tx_in["txouthash"] = codecs.decode(flip_byte_order(prv_txid), 'hex')
+rtx.tx_in["tx_out_index"] = struct.pack("<L", 0)
+rtx.tx_in["script"] = codecs.decode(
+    "76a914"+sender_hashed_pubkey+"88ac", 'hex')
+rtx.tx_in["scrip_bytes"] = struct.pack("<B", len(rtx.tx_in["script"]))
+rtx.tx_in["sequence"] = codecs.decode("ffffffff", 'hex')
+
+
+rtx.tx_out1["value"] = struct.pack("<Q", 50000)
+rtx.tx_out1["pk_script"] = codecs.decode(
+    "76a914"+receiver_hashed_pubkey+"88ac", 'hex')
+rtx.tx_out1["pk_script_bytes"] = struct.pack(
+    "<B", len(rtx.tx_out1["pk_script"]))
+
+rtx.tx_out2["value"] = struct.pack("<Q", 40000)
+rtx.tx_out2["pk_script"] = codecs.decode(
+    "76a914"+sender_hashed_pubkey+"88ac", 'hex')
+rtx.tx_out2["pk_script_bytes"] = struct.pack(
+    "<B", len(rtx.tx_out2["pk_script"]))
+
+raw_tx_string = (
+
+    rtx.version
+    + rtx.tx_in_count
+    + rtx.tx_in["txouthash"]
+    + rtx.tx_in["tx_out_index"]
+    + rtx.tx_in["scrip_bytes"]
+    + rtx.tx_in["script"]
+    + rtx.tx_in["sequence"]
+    + rtx.tx_out_count
+    + rtx.tx_out1["value"]
+    + rtx.tx_out1["pk_script_bytes"]
+    + rtx.tx_out1["pk_script"]
+    + rtx.tx_out2["value"]
+    + rtx.tx_out2["pk_script_bytes"]
+    + rtx.tx_out2["pk_script"]
+    + rtx.lock_time
+    + struct.pack("<L", 1)
+
+)
+
+hashed_tx_to_sign = hashlib.sha256(
+    hashlib.sha256(raw_tx_string).digest()).digest()
+sk = ecdsa.SigningKey.from_string(codecs.decode(
+    sender_private_key, 'hex'), curve=ecdsa.SECP256k1)
+vk = sk.verifying_key
+public_key = (b'04' + vk.to_string()).hex()
+signature = sk.sign_digest(
+    hashed_tx_to_sign, sigencode=ecdsa.util.sigencode_der_canonize)
+
+
+sigscript = (
+
+    signature
+    + b"01"
+    + struct.pack("<B", len(codecs.decode(public_key, 'hex')))
+    + codecs.decode(public_key, 'hex')
+
+)
+
+
+real_tx = (
+    rtx.version
+    + rtx.tx_in_count
+    + rtx.tx_in["txouthash"]
+    + rtx.tx_in["tx_out_index"]
+    + struct.pack("<B", len(sigscript) + 1)
+    + struct.pack("<B", len(signature) + 1)
+    + sigscript
+    + rtx.tx_in["sequence"]
+    + rtx.tx_out_count
+    + rtx.tx_out1["value"]
+    + rtx.tx_out1["pk_script_bytes"]
+    + rtx.tx_out1["pk_script"]
+    + rtx.tx_out2["value"]
+    + rtx.tx_out2["pk_script_bytes"]
+    + rtx.tx_out2["pk_script"]
+    + rtx.lock_time
+)
+
+print("raw transaction in hex form => ", real_tx.hex())
+
+hash_1 = hashlib.sha256(real_tx).digest()
+hash_2 = hashlib.sha256(hash_1).digest()
+
+print("\ntx hash => ", flip_byte_order(hash_2.hex()), "\n")
